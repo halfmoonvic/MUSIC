c22a16a86d316b33c1152becba04f26234eebf41
halfmoon <halfmoonvic@gmail.com>
2017-07-26 08:28:02 +0800

    7.26 上班 已获取的歌手的详细数据

--- vue-music/src/components/singer-detail/singer-detail.vue
+++ vue-music/src/components/singer-detail/singer-detail.vue
@@ -6,6 +6,8 @@
 
 <script>
 import {mapGetters} from 'vuex'
+import {getSingerDetail} from 'api/singer'
+import {ERR_OK} from 'api/config'
 
 export default {
     name: 'singer-detail',
@@ -20,12 +22,26 @@ export default {
         };
     },
     created() {
+        this._getDetail()
         console.log(this.singer)
     },
     computed: {
         ...mapGetters([
             'singer'
         ])
+    },
+    methods: {
+        _getDetail() {
+            if (!this.singer.id) {
+                this.$router.push('/singer')
+                return
+            }
+            getSingerDetail(this.singer.id).then( (res) => {
+                if (res.code === ERR_OK) {
+                    console.log(res.data);
+                }
+            } )
+        }
     }
 };
 </script>
