a8f96420dbd1eeaf51d0616d44201f8119f56caf
cs-300 <halfmoonvic@gmail.com>
2017-09-04 20:01:41 +0800

    nothing

--- vue-music/src/common/js/song.js
+++ vue-music/src/common/js/song.js
@@ -20,7 +20,6 @@ export default class Song {
         }
         return new Promise((resolve, reject) => {
             getLyric(this.mid).then((res) => {
-                console.log(res)
                 if (res.retcode === ERR_OK) {
                     this.lyric = Base64.decode(res.lyric)
                     resolve(this.lyric)
